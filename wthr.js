a76988ace779f5fe6212ae5bff8af844563b677e
Ivan Antic <47058987+pera-kojot-genije@users.noreply.github.com>
2019-03-04 03:58:02 +0100

    Add files via upload

---
+++ wthr.js
@@ -0,0 +1,31 @@
+// Pozadina gornjeg dela treba da se menja u zavisnosti od vremenskih prilika. Kodovi za gradiente su:
+
+// Za oblačno ili snežno vreme: 
+// background: rgb(81,103,126);
+// background: linear-gradient(180deg, rgba(81,103,126,1) 0%, rgba(166,179,192,1) 40%, rgba(166,179,192,1) 100%);
+
+// Za vedro vreme: 
+// background: rgb(47,105,164);
+// background: linear-gradient(180deg, rgba(47,105,164,1) 0%, rgba(92,168,246,1) 100%);
+
+// Za grmljavinu i nevreme: 
+// background: rgb(0,0,0);
+// background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(60,74,88,1) 100%);
+
+// Za kišu: 
+// background: rgb(4,9,14);
+// background: linear-gradient(180deg, rgba(4,9,14,1) 0%, rgba(44,95,146,1) 100%);
+
+//-----------------------------------------------------------------------------------
+
+// Nazivi slika su:
+
+// cloudy.png
+// rainy.png
+// partly_cloudy.png
+// sunny.png
+// windy.png
+// snowy.png
+// stormy.png
+
+
